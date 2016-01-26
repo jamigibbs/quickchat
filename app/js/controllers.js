0555f8fe320759bce1a719d263294242f4bac74a
JamiGibbs <jami0821@gmail.com>
2016-01-26 08:07:29 -0600

    Adding ability to submit new chat rooms

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,4 +4,16 @@ angular.module('quickchatApp')
 
     $scope.rooms = roomsService.all;
 
+    $scope.addRoom = function() {
+
+      roomsService.all.$add({
+        name: this.room,
+        created_at: Date.now()
+      });
+
+      // Reset the variable for the next room submission
+      this.room = null;
+
+    };
+
   }]);
