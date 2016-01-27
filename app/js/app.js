f4b0cd7d69e547434bdaa124d773bc2157071913
JamiGibbs <jami0821@gmail.com>
2016-01-27 14:02:56 -0600

    Removing duplicate require

--- app/js/app.js
+++ app/js/app.js
@@ -3,7 +3,6 @@ require('angular-ui-router');
 require('firebase');
 require('angularfire');
 require('angular-cookies');
-require('angular-cookies');
 require('ng-dialog/js/ngDialog');
 
 require('./config');
